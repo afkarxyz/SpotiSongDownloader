87657644f2d190127027eca0589426518ff5dad2
afkarxyz <mzamzamafkarhadiq@gmail.com>
2025-02-05 07:09:40 +0700

    Update v2.1

--- getTracks.py
+++ getTracks.py
@@ -24,7 +24,7 @@ def get_url(track_info):
     
     headers = {
         'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Cookie': 'PHPSESSID=j1ckcp3uapkmdhs22htg11fqvf; _ga=GA1.1.15078990.1738683269; _ga_X67PVRK9F0=GS1.1.1738683268.1.1.1738683277.0.0.0; quality=m4a',
+        'Cookie': 'PHPSESSID=j1ckcp3uapkmdhs22htg11fqvf; quality=m4a',
         'Origin': 'https://spotisongdownloader.to',
         'Referer': 'https://spotisongdownloader.to/',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
